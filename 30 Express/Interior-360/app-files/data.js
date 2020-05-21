5f700c2b5e04c69f10d976835c2fe8075ef90bb7
takbeom <tak@yachtdesignworks.com>
2020-05-21 10:48:46 -0400

    Update

--- 30 Express/Interior-360/app-files/data.js
+++ 30 Express/Interior-360/app-files/data.js
@@ -24,20 +24,20 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 1.5519698188828244,
-        "pitch": 0.2149136450179423,
+        "yaw": 1.5429813267285049,
+        "pitch": 0.26938846775125036,
         "fov": 1.3806389203442926
       },
       "linkHotspots": [
         {
-          "yaw": 1.2011316116360984,
-          "pitch": 0.2151259498413154,
+          "yaw": 1.191275736098893,
+          "pitch": 0.2178844290471229,
           "rotation": 0,
           "target": "1-vanity"
         },
         {
-          "yaw": 1.717248767420375,
-          "pitch": 0.22049056120787824,
+          "yaw": 1.7128243575711828,
+          "pitch": 0.10177985142532009,
           "rotation": 0,
           "target": "2-head"
         }
@@ -68,20 +68,20 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 1.3208254824585826,
-        "pitch": 0.7533829122495206,
+        "yaw": 1.5795997694397075,
+        "pitch": 0.878102372158839,
         "fov": 1.3806389203442926
       },
       "linkHotspots": [
         {
-          "yaw": -2.211493530170287,
-          "pitch": 0.7347753608665712,
+          "yaw": -2.061154034014473,
+          "pitch": 0.8132205377895048,
           "rotation": 0,
           "target": "2-head"
         },
         {
-          "yaw": -0.795180021092694,
-          "pitch": 0.4041975737464707,
+          "yaw": -0.6443704063201245,
+          "pitch": 0.40683466719154104,
           "rotation": 0,
           "target": "0-v-birth"
         }
@@ -112,14 +112,14 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 1.56897887649943,
-        "pitch": 0.7673094123243871,
+        "yaw": 1.5714598403948257,
+        "pitch": 0.808216774547974,
         "fov": 1.3806389203442926
       },
       "linkHotspots": [
         {
-          "yaw": 2.2031879247167394,
-          "pitch": 0.2833211975083465,
+          "yaw": 2.2783266901777566,
+          "pitch": 0.33345983583003225,
           "rotation": 0,
           "target": "1-vanity"
         }
@@ -127,11 +127,11 @@ var APP_DATA = {
       "infoHotspots": []
     }
   ],
-  "name": "30 EXPRESS - INTERIOR",
+  "name": "30 EXPRESS -INTERIOR",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": false,
-    "fullscreenButton": true,
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
     "viewControlButtons": false
   }
 };
